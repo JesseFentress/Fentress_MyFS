320807d8d37fdb195f2e53b501d469500e767878
Jesse Fentress <jxf5627@L7ABWLD342-203>
2023-03-22 12:04:42 -0400

    Updated events

--- Fentress_MyFS/Form1.Designer.cs
+++ Fentress_MyFS/Form1.Designer.cs
@@ -29,72 +29,101 @@
         private void InitializeComponent()
         {
             this.btnAdd = new System.Windows.Forms.Button();
-            this.labelFileName = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.lstSectors = new System.Windows.Forms.ListBox();
             this.txtFileName = new System.Windows.Forms.TextBox();
             this.chckHidden = new System.Windows.Forms.CheckBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtFileSize = new System.Windows.Forms.TextBox();
             this.btnExit = new System.Windows.Forms.Button();
             this.btnRemove = new System.Windows.Forms.Button();
             this.chckReadOnly = new System.Windows.Forms.CheckBox();
-            this.labelFileSize = new System.Windows.Forms.Label();
+            this.lblFileSize = new System.Windows.Forms.Label();
+            this.lblSectorSize = new System.Windows.Forms.Label();
+            this.txtSectorSize = new System.Windows.Forms.TextBox();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.txtStatusLine = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnAdd
             // 
             this.btnAdd.Enabled = false;
-            this.btnAdd.Location = new System.Drawing.Point(442, 24);
+            this.btnAdd.Location = new System.Drawing.Point(18, 218);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(99, 23);
             this.btnAdd.TabIndex = 0;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
-            // labelFileName
+            // lblFileName
             // 
-            this.labelFileName.AutoSize = true;
-            this.labelFileName.Location = new System.Drawing.Point(17, 28);
-            this.labelFileName.Name = "labelFileName";
-            this.labelFileName.Size = new System.Drawing.Size(60, 15);
-            this.labelFileName.TabIndex = 1;
-            this.labelFileName.Text = "File Name";
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(17, 95);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(60, 15);
+            this.lblFileName.TabIndex = 1;
+            this.lblFileName.Text = "File Name";
             // 
-            // listBox1
+            // lstSectors
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(587, 12);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(201, 424);
-            this.listBox1.TabIndex = 2;
+            this.lstSectors.Font = new System.Drawing.Font("Lucida Sans Unicode", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lstSectors.FormattingEnabled = true;
+            this.lstSectors.ItemHeight = 16;
+            this.lstSectors.Items.AddRange(new object[] {
+            "1 >",
+            "2 >",
+            "3 >",
+            "4 >",
+            "5 >",
+            "6 >",
+            "7 >",
+            "8 >",
+            "9 >",
+            "0 >",
+            "1 >",
+            "2 >",
+            "3 >",
+            "4 >",
+            "5 >",
+            "6 >",
+            "7 >",
+            "8 >",
+            "9 >",
+            "0 >"});
+            this.lstSectors.Location = new System.Drawing.Point(587, 12);
+            this.lstSectors.Name = "lstSectors";
+            this.lstSectors.Size = new System.Drawing.Size(201, 420);
+            this.lstSectors.TabIndex = 2;
             // 
             // txtFileName
             // 
-            this.txtFileName.Location = new System.Drawing.Point(17, 50);
+            this.txtFileName.Location = new System.Drawing.Point(17, 117);
             this.txtFileName.Name = "txtFileName";
             this.txtFileName.Size = new System.Drawing.Size(100, 23);
             this.txtFileName.TabIndex = 3;
+            this.txtFileName.TextChanged += new System.EventHandler(this.txtFileName_TextChanged);
             // 
             // chckHidden
             // 
             this.chckHidden.AutoSize = true;
-            this.chckHidden.Location = new System.Drawing.Point(289, 32);
+            this.chckHidden.Location = new System.Drawing.Point(277, 106);
             this.chckHidden.Name = "chckHidden";
             this.chckHidden.Size = new System.Drawing.Size(65, 19);
             this.chckHidden.TabIndex = 4;
             this.chckHidden.Text = "Hidden";
             this.chckHidden.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // txtFileSize
             // 
-            this.textBox2.Location = new System.Drawing.Point(146, 50);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 23);
-            this.textBox2.TabIndex = 5;
+            this.txtFileSize.Location = new System.Drawing.Point(146, 117);
+            this.txtFileSize.Name = "txtFileSize";
+            this.txtFileSize.Size = new System.Drawing.Size(100, 23);
+            this.txtFileSize.TabIndex = 5;
+            this.txtFileSize.TextChanged += new System.EventHandler(this.txtFileSize_TextChanged);
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(442, 111);
+            this.btnExit.Location = new System.Drawing.Point(18, 305);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(99, 23);
             this.btnExit.TabIndex = 6;
@@ -103,7 +132,7 @@
             // 
             // btnRemove
             // 
-            this.btnRemove.Location = new System.Drawing.Point(442, 68);
+            this.btnRemove.Location = new System.Drawing.Point(18, 262);
             this.btnRemove.Name = "btnRemove";
             this.btnRemove.Size = new System.Drawing.Size(99, 23);
             this.btnRemove.TabIndex = 7;
@@ -114,36 +143,76 @@
             // chckReadOnly
             // 
             this.chckReadOnly.AutoSize = true;
-            this.chckReadOnly.Location = new System.Drawing.Point(289, 68);
+            this.chckReadOnly.Location = new System.Drawing.Point(277, 131);
             this.chckReadOnly.Name = "chckReadOnly";
             this.chckReadOnly.Size = new System.Drawing.Size(80, 19);
             this.chckReadOnly.TabIndex = 8;
             this.chckReadOnly.Text = "Read Only";
             this.chckReadOnly.UseVisualStyleBackColor = true;
             // 
-            // labelFileSize
+            // lblFileSize
             // 
-            this.labelFileSize.AutoSize = true;
-            this.labelFileSize.Location = new System.Drawing.Point(146, 28);
-            this.labelFileSize.Name = "labelFileSize";
-            this.labelFileSize.Size = new System.Drawing.Size(48, 15);
-            this.labelFileSize.TabIndex = 9;
-            this.labelFileSize.Text = "File Size";
+            this.lblFileSize.AutoSize = true;
+            this.lblFileSize.Location = new System.Drawing.Point(146, 95);
+            this.lblFileSize.Name = "lblFileSize";
+            this.lblFileSize.Size = new System.Drawing.Size(48, 15);
+            this.lblFileSize.TabIndex = 9;
+            this.lblFileSize.Text = "File Size";
+            // 
+            // lblSectorSize
+            // 
+            this.lblSectorSize.AutoSize = true;
+            this.lblSectorSize.Location = new System.Drawing.Point(16, 34);
+            this.lblSectorSize.Name = "lblSectorSize";
+            this.lblSectorSize.Size = new System.Drawing.Size(63, 15);
+            this.lblSectorSize.TabIndex = 10;
+            this.lblSectorSize.Text = "Sector Size";
+            this.lblSectorSize.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtSectorSize
+            // 
+            this.txtSectorSize.Location = new System.Drawing.Point(85, 31);
+            this.txtSectorSize.Name = "txtSectorSize";
+            this.txtSectorSize.Size = new System.Drawing.Size(50, 23);
+            this.txtSectorSize.TabIndex = 11;
+            this.txtSectorSize.Text = "500";
+            this.txtSectorSize.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Location = new System.Drawing.Point(123, 266);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(13, 15);
+            this.labelCount.TabIndex = 12;
+            this.labelCount.Text = "0";
+            this.labelCount.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // txtStatusLine
+            // 
+            this.txtStatusLine.Location = new System.Drawing.Point(18, 413);
+            this.txtStatusLine.Name = "txtStatusLine";
+            this.txtStatusLine.Size = new System.Drawing.Size(538, 23);
+            this.txtStatusLine.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.labelFileSize);
+            this.Controls.Add(this.txtStatusLine);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.txtSectorSize);
+            this.Controls.Add(this.lblSectorSize);
+            this.Controls.Add(this.lblFileSize);
             this.Controls.Add(this.chckReadOnly);
             this.Controls.Add(this.btnRemove);
             this.Controls.Add(this.btnExit);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtFileSize);
             this.Controls.Add(this.chckHidden);
             this.Controls.Add(this.txtFileName);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.labelFileName);
+            this.Controls.Add(this.lstSectors);
+            this.Controls.Add(this.lblFileName);
             this.Controls.Add(this.btnAdd);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -155,14 +224,18 @@
         #endregion
 
         private Button btnAdd;
-        private Label labelFileName;
-        private ListBox listBox1;
+        private Label lblFileName;
+        private ListBox lstSectors;
         private TextBox txtFileName;
         private CheckBox chckHidden;
-        private TextBox textBox2;
+        private TextBox txtFileSize;
         private Button btnExit;
         private Button btnRemove;
         private CheckBox chckReadOnly;
-        private Label labelFileSize;
+        private Label lblFileSize;
+        private Label lblSectorSize;
+        private TextBox txtSectorSize;
+        private Label labelCount;
+        private TextBox txtStatusLine;
     }
 }
--- Fentress_MyFS/Form1.cs
+++ Fentress_MyFS/Form1.cs
@@ -11,5 +11,63 @@ namespace Fentress_MyFS
         {
             btnAdd.Enabled = !btnAdd.Enabled;
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFileName_TextChanged(object sender, EventArgs e)
+        {
+            CheckAddEnable();
+        }
+
+        private void txtFileSize_TextChanged(object sender, EventArgs e)
+        {
+            CheckAddEnable();
+        }
+
+        private void CheckAddEnable()
+        {
+            if ((txtFileName.TextLength > 0) && (txtFileSize.TextLength > 0))
+            {
+                btnAdd.Enabled = true;
+            }
+            else
+            {
+                btnAdd.Enabled = false;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string fileInfo;
+            char hiddenChar = 'h';
+            char readOnlyChar = 'r';
+
+            if (chckHidden.Checked == true)
+            {
+                hiddenChar = 'H';
+            }
+
+            if (chckReadOnly.Checked == true)
+            {
+                readOnlyChar = 'R';
+            }
+
+            fileInfo = txtFileName.Text + ", " + txtFileSize.Text + ", " + hiddenChar + ", " + readOnlyChar;
+
+            lstSectors.Items.Add(fileInfo);
+        }
     }
 }
